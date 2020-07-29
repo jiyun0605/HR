efe335d041b274f920650eb09206750a7f18e5ad
kanghanna1202 <namnam1178@daum.net>
2020-07-29 22:39:05 +0900

    fix create, url

---
+++ HealthRoutine/routine/urls.py
@@ -0,0 +1,8 @@
+# from django.conf.urls import url
+# from django.urls import path, include
+# from . import views
+#
+# urlpatterns = [
+#     # path('create/', views.create_view),
+#     url(r'^$', views.Post, name='post'),
+# ]
--- HealthRoutine/routine/views.py
+++ HealthRoutine/routine/views.py
@@ -1,32 +1,35 @@
-# import json
-#
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework import status
-# from rest_framework.decorators import api_view, permission_classes
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework.response import Response
-# from time import timezone
-# from .models import Post
-# from .serializers import PostSerializer
-#
-# @api_view(["POST"])
-# @csrf_exempt
-# @permission_classes([IsAuthenticated])
-# def create_view(request):
-#     payload = json.loads(request.body)
-#     user = request.user
-#     try:
-#         post = Post.objects.create(
-#             title=payload["title"],
-#             sports_name=payload["sports_name"],
-#             routine=payload["routine"],
-#             sets=payload["sets"],
-#             created_date=payload["created_date"]
-#
-#
-#         )
+import json
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
+
+@api_view(["POST"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def create_view(request):
+    payload = json.loads(request.body)
+    user = request.user
+    try:
+        post = Post.objects.create(
+            title=payload["title"],
+            sports_name=payload["sports_name"],
+            routine=payload["routine"],
+            sets=payload["sets"],
+            created_date=payload["created_date"],
+            updated_date=payload["updated_date"]
+        )
+        serializer = PostSerializer(post)
+        return JsonResponse({'post': serializer.data}, safe=False, status=status.HTTP_201_CREATED)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+    except Exception:
+        return JsonResponse({'error':'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # def createView(request):
 #     if request.method == 'POST':
