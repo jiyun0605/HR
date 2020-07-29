b7bc45d772577875cddafb5575e96aa30d1383fb
kanghanna1202 <namnam1178@daum.net>
2020-07-29 19:28:00 +0900

    Feature: -create models

--- HealthRoutine/HealthRoutine/settings.py
+++ HealthRoutine/HealthRoutine/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'routine.apps.RoutineConfig'
+    'routine.apps.RoutineConfig',
 ]
 
 MIDDLEWARE = [
--- HealthRoutine/routine/models.py
+++ HealthRoutine/routine/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField('제목', max_length=10)
+    sports_name = models.CharField('운동 이름', max_length=100)
+    routine = models.IntegerField('루틴', default=0)
+    sets = models.IntegerField('세트', default=0)
+    created_date = models.DateTimeField('생성 날짜', auto_now_add=True)
+    updated_date = models.DateTimeField('수정 날짜', auto_now_add=True)
+
+    def __str__(self):
+        return '[{}] {}'.format(self.id, self.title)
